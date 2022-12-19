986befa020bd5d0011ab139f24ba8922d547cf4f
DESKTOP-ND7LLFQ\bkasp <b.kasperowicz@talmundo.com>
2022-12-19 13:50:07 +0100

    typo fix

--- src/main/java/com/javaexcercises/examples/DBSelectExample.java
+++ src/main/java/com/javaexcercises/examples/DBSelectExample.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DBSelectExcample {
+public class DBSelectExample {
     public static void main(String[] args) {
         Connection connection = null;
 
